43539634f6a5d8c8a3be39e9102e689b394be5bd
somq14 <54357320+somq14@users.noreply.github.com>
2020-02-09 18:28:23 +0900

    redux-sagaで排他制御をするサンプル

---
+++ saga-semaphore/src/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from "redux"
+import createSagaMiddleware from "redux-saga"
+
+import reducer from "./reducer"
+
+export const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+export default store
